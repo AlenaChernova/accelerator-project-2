1084917a1f2e4cc6177936e2988178cc36732c62
Alena Chernova <alenvlasovav@gmail.com>
2025-04-22 14:38:32 +0300

    Стилизация и оживление хедера и херо

---
+++ source/js/header.js
@@ -0,0 +1,38 @@
+const openButton = document.querySelector('.page-header__burger-button');
+const header = document.querySelector('.page-header');
+const links = header.querySelectorAll('.page-header__link');
+const firstTabButton = header.querySelector('.page-header__burger-button');
+
+let onDocumentFocus;
+
+const openMenu = () => {
+  header.classList.add('is-open');
+  document.body.setAttribute('style', 'overflow: hidden');
+  onDocumentFocus = (event) => {
+    if (!header.contains(event.target)) {
+      event.stopPropagation();
+      firstTabButton.focus();
+    }
+  };
+  document.addEventListener('focus', onDocumentFocus, true);
+};
+
+const closeMenu = () => {
+  document.body.removeAttribute('style');
+  header.classList.remove('is-open');
+  document.removeEventListener('focus', onDocumentFocus, true);
+};
+
+openButton.addEventListener('click', () => {
+  if (header.classList.contains('is-open')) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
+});
